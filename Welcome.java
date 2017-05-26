15b4e1ccf1f6787fb22fcd389a9b81156b906a32
gxl <gaoxiaolong@hymost.com>
2017-05-26 09:49:24 +0800

    [Core Java]Welcome

---
+++ Welcome.java
@@ -0,0 +1,13 @@
+public class Welcome
+{
+	public static void main(String[] args )
+	{
+		String[] greeting = new String[3];
+		greeting[0] = "Welcome to Core java";
+		greeting[1] = "by Cay Horstmann";
+		greeting[2] = "and Gary Cornell";
+
+		for (String g : greeting)
+			System.out.println(g);
+	}
+}
